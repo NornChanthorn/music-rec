a800dabee74b380d5c97b4cc09aa03ccb6084195
NornChanthorn <nornchanthorn660@gmail.com>
2025-07-20 17:46:02 +0700

    choore: config prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,50 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+// eslint.config.js
+import js from "@eslint/js"
+import ts from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactNative from "eslint-plugin-react-native"
+import prettier from "eslint-plugin-prettier"
 
-module.exports = defineConfig([
-  expoConfig,
+export default [
+  js.configs.recommended,
   {
-    ignores: ['dist/*'],
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+      react,
+      "react-hooks": reactHooks,
+      "react-native": reactNative,
+      prettier,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...reactNative.configs.all.rules,
+      "react/react-in-jsx-scope": "off",
+      "react-native/no-inline-styles": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "prettier/prettier": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
-]);
+  {
+    ignores: ["node_modules", "dist", "build"],
+  },
+]
